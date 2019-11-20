71cd5cbbdac4d695160c6c07e9757731874d08d7
Luana Faria Lane <luanasfff@gmail.com>
2019-11-20 01:50:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,11 @@
 # Your code goes here!
 class Dog 
-   def name 
-     name = "Fido"
+   def name=(dogs_name)
+     this_dogs_name = dogs_name
     end 
-    
+   def name
+     name.Fido
+     
      
    def bark 
     puts "Woof!"
